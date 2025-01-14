a56049378e751f53a53326507c548c2ac036f4cc
giorgiospirostopala <gio.topala@gmail.com>
2025-01-14 18:49:20 +0100

    add: cicle

---
+++ main.js
@@ -0,0 +1,14 @@
+// Scrivi un programma che stampi i numeri da 1 a 100
+
+// • Per i multipli di 3 stampi “Fizz” al posto del numero
+// • Per i multipli di 5 stampi Buzz
+// • Per i multipli sia di 3 sia di 5 stampi FizzBuzz
+
+
+// ciclo da 1 a 100
+for (let i = 1; i <=100; i++){
+    
+    console.log(i);
+
+}
+
