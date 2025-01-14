77e4778ba5d64b76439aa0d96020bc0cd496dedb
giorgiospirostopala <gio.topala@gmail.com>
2025-01-14 18:52:49 +0100

    add: scaletta-guida

--- main.js
+++ main.js
@@ -4,6 +4,13 @@
 // • Per i multipli di 5 stampi Buzz
 // • Per i multipli sia di 3 sia di 5 stampi FizzBuzz
 
+// - creo il ciclo
+// - creo i vari IF
+//      - SE il giro di ciclo è divisibile per 3 -> stampo "Fizz"
+//      - SE il giro di ciclo è divisibile per 5 -> stampo "Buzz"
+//      - SE il giro di ciclo è divisibile per 3 && 5 -> stampo "FizzBuzz"
+//      - ALTRIMENTI non stampo nulla (credo?)
+
 
 // ciclo da 1 a 100
 for (let i = 1; i <=100; i++){
