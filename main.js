35930e3988b54d73c7e9c9b55ad92af7d5fe77c7
giorgiospirostopala <gio.topala@gmail.com>
2025-01-14 19:19:25 +0100

    add: risoluzione gerarchica (fine)

--- main.js
+++ main.js
@@ -15,17 +15,17 @@
 
 for (let i = 1; i <= 100; i++){
     
-    if(i % 3 == 0){
+    if(i % 3 == 0 && i % 5 == 0){
 
-        console.log("Fizz");
+        console.log("FizzBuzz");
 
     } else if(i % 5 == 0) {
 
         console.log("Buzz");
 
-    } else if(i % 3 == 0 && i % 5 == 0){
+    } else if(i % 3 == 0){
 
-        console.log("FizzBuzz");
+        console.log("Fizz");
 
     } else {
 
